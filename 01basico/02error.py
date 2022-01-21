13cb580bc67987f28d8f75364ef84dd449d6a430
cerebroBin <cerebrobin@hotmail.com>
2022-01-21 10:51:07 -0600

    02error

---
+++ 01basico/02error.py
@@ -0,0 +1,80 @@
+# en caso de error, no desesperes,
+# calmate, analiza donde esta el
+# error y piensa como solucionarlo
+
+# la solucion la puedes encontar tu
+# tambien puedes pedir ayuda a
+# un amigo o a en internet
+
+# una vez tengas la solucion,
+# aplicala y en caso de error, no
+# desesperes, calmate, analiza ...
+
+# y asi hasta arreglar el error
+
+# los errores que se pueden
+# presentar en un programa basico
+# que mande un mensaje a pantalla
+# son los siguientes
+
+# python es un lenguaje sensible
+# a mayusculas y minusculas
+
+# por lo que print no es los mismo
+# que Print o Print o PrInT o
+# variaciones
+
+# existen lenguajes que no son
+# sensibles a mayusculas y
+# minusculas como pascal, pero
+# no es el caso de python
+
+# si no se escribe print, todo en
+# minusculas, marcara error
+
+# si no usas parentesis en python 3
+# marcara error, en python 2 y 1
+# no son necesarios
+
+# si solo usas el parentesis de
+# inicio ( pero no el de cierre )
+
+# si solo usas el parentesis de
+# cierre ) pero no el de inicio (
+
+# si solo usas la comilla simple '
+# de inico del texto pero no de
+# cierre del texto
+
+# si solo usas la comilla doble "
+# de fin del texto pero no de
+# inicio del texto
+
+# si mezclas comilla simple con
+# comilla doble '" o "'
+
+# si no colocas en orden los
+# parentesis y las comillas
+# por ejemplo ('texto)' esta mal
+
+# otro ejemplo de error al no
+# colocar en orden los parentesis
+# por ejemplo "(texto")
+
+# tambien se pueden generar otros
+# errores al colocar en desorden
+# los parentesis y comillas
+
+# como vez hasta en los programas
+# mas simples se puedem generar
+# una gran cantidad de errores
+
+# sin embargo en caso de error
+# calmate, analiza, y piensa como
+# resolver el error ... y asi hasta
+# que el programa quede sin errores
+
+# el siguiente programa manda un
+# mensaje a pantalla
+
+print('manda mensaje a pantalla')
