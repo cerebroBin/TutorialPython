ae0e5091565927b4128d9c61ea205a10e8025ccc
cerebroBin <cerebrobin@hotmail.com>
2022-01-21 10:46:26 -0600

    01mensaje

---
+++ 01basico/01mensaje.py
@@ -0,0 +1,26 @@
+#esto es un comentario
+#y no afecta al programa
+#puedes poner todos los que quieras
+
+#puedes separar los comentarios
+#con saltos de lineas y espacios
+
+# en python se usa el simbolo #
+# para poner comentarios
+
+# en otros lenguajes se usan
+# otros simbolos
+
+# sirven para dejar notas utiles
+
+# vamos a crear un programa que
+# manda un mensaje a pantalla
+# con la instruccion print
+
+# el texto va entre '' o ""
+# no mezclar por que genera error
+
+# finalmente 1 linea de codigo
+# que manda un mensaje a pantalla
+
+print('manda mensaje a pantalla')
